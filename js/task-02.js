6d44596cd9df1f1d98a8ea9eee907c0ea0090da0
MiaktynovS <ousm9595@gmail.com>
2022-11-03 22:35:28 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+const elements = ingredients.map(ingredient => {
+  const ingredientsItemEl = document.createElement('li');
+  ingredientsItemEl.textContent = ingredient;
+  ingredientsItemEl.classList.add('item');
+  
+  return ingredientsItemEl;
+});
+
+ingredientsEl.append(...elements);
+console.log(ingredientsEl);
