9b29030ab00216044f4ce18416ccbf1251da31a7
MiaktynovS <ousm9595@gmail.com>
2022-11-03 15:49:23 +0200

    Update task-01

---
+++ js/task-01.js
@@ -0,0 +1,12 @@
+
+const numberOfCategories = document.querySelectorAll('.item');
+console.log(`Number of categories: ${numberOfCategories.length}`);
+
+const categoriesArr = [...numberOfCategories]
+    .map(
+        categories => `Category: ${categories.children[0].textContent}
+    Elements: ${ categories.children[1].children.length }`)
+      .join("\n");
+      console.log(categoriesArr);
+
+
