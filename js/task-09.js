3bf178426e67a8e459b6b7f833fe762226e99785
MiaktynovS <ousm9595@gmail.com>
2022-11-07 16:18:14 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,25 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+
+const refs = {
+  btnEl: document.querySelector('.change-color'),
+  spanEl: document.querySelector('.color'),
+  bodyEl: document.querySelector('body'),
+}
+console.log(refs.btnEl);
+console.log(refs.spanEl);
+console.log(refs.bodyEl);
+
+refs.btnEl.addEventListener('click', onTakeBtn)
+
+function onTakeBtn() {
+  refs.spanEl.textContent = getRandomHexColor();
+  refs.bodyEl.style.backgroundColor = getRandomHexColor();
+}
+
+
+
+
