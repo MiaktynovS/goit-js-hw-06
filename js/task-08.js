f3ea7609230812334e6f5052d1d2410b67ae5361
MiaktynovS <ousm9595@gmail.com>
2022-11-07 15:12:34 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,28 @@
+const formEl = document.querySelector('.login-form');
+console.log(formEl);
+const inputs = document.querySelectorAll('input');
+console.log(inputs);
+formEl.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    console.log(formData);
+    
+    inputs.forEach(input => {
+        if (input.value === "") {
+            alert("Всі поля повині бути заповнені")
+            return console.log(false)
+        }
+        else {
+            console.log(true)
+        }
+    }) 
+    
+    formEl.reset();
+   } 
+ 
+
+
 
